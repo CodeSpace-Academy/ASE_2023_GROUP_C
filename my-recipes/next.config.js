78b9de472df18f56b7fc1658a39cfc1154c2387b
keotshepileMaje <keotshepilemaje@gmail.com>
2023-10-10 09:55:30 +0200

    Initialized mongodb using .env file variables

--- my-recipes/next.config.js
+++ my-recipes/next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    mongodb_username: 'keotshepilemaje',
+    mongodb_password: 'RjZymySXvwjI3jFk',
+    mongodb_clustername: 'cluster0',
+    mongodb_database: 'dev-rep',
+  }
 }
 
 module.exports = nextConfig
