44bf4ffa8d1f9b3220c0925afe16742bb24d98a8
keotshepileMaje <keotshepilemaje@gmail.com>
2023-10-10 15:02:08 +0200

    Added recipe API

--- pages/api/recipes/[page].js
+++ pages/api/recipes/[page].js
@@ -1,3 +1,28 @@
-export default function handler( req, res){
+// import { connectToDb, getAllRecipes } from "@/utils/mongodb-utils";
 
+import { connectToDb, getAllRecipes } from "../../../utils/mongodb-utils";
+
+export default async function handler(req, res) {
+    let client;
+    
+    try {
+        client = await connectToDb()
+    } catch(error) {
+        res.status(500).json({message: 'Database connection failed'})
+        return 
+    }
+
+    if (req.method === 'GET') {
+        try {
+            const recipeDocuments = await getAllRecipes(
+                client,
+                'recipes',
+                {_id: -1},
+                1,
+            )
+            res.status(200).json({message: recipeDocuments})
+        } catch (erorr) {
+            res.status(500).json({message: 'Getting recipes failed'})
+        }
+    }
 }
