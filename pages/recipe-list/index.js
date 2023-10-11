c8938499b6322e61a3c00ac2dade015632b81743
LentsweLetlotloMokotedi <lentsweletlotlo0@gmail.com>
2023-10-11 16:21:13 +0200

    styled receipt list a bit

--- pages/recipe-list/index.js
+++ pages/recipe-list/index.js
@@ -2,46 +2,41 @@
 
 import { connectToDb, getAllRecipes } from "../../utils/mongodb-utils";
 
-export async function getStaticProps () {
-  let client = await connectToDb()
 
-          const recipeDocuments = await getAllRecipes(
-              client,
-              'recipes',
-              {_id: -1},
-              1,
-          )
+export async function getStaticProps() {
+  let client = await connectToDb();
+
+  const recipeDocuments = await getAllRecipes(
+    client,
+    'recipes',
+    { _id: -1 },
+    1
+  );
 
   return {
     props: {
-      recipes: recipeDocuments
-    }
-  }
-
+      recipes: recipeDocuments,
+    },
+  };
 }
-
 
 export default function RecipeList(props) {
   const { recipes } = props;
-  if ( !recipes ) return <p>Loading...</p>
+  if (!recipes) return <p>Loading...</p>;
   return (
     <div>
       <h1>Recipe List</h1>
       <ul>
         {recipes.map((recipe) => (
-          
           <li key={recipe._id}>
-            {recipe.images.map((imageUrl, index) => (
-            <img key={index} src={imageUrl} alt={recipe.title} />
-          ))}
+            {recipe.images.length > 0 && (
+              <img src={recipe.images[0]} alt={recipe.title} height="250" width="250" /> // Display the first image
+            )}
             <h2>{recipe.title}</h2>
             <p>{recipe.description}</p>
-            
-           
           </li>
         ))}
       </ul>
     </div>
   );
 }
-
