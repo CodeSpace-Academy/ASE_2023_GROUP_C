97a5a134b2df4f6e7d38c5d35162812f12d94b19
Kealeboga Letlhake <letlhake.kv@gmail.com>
2023-10-11 16:05:06 +0200

    Add Navbar
    I created a navbar that will appear on the home page and in the recipe-list page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
-import RecipeList from "./recipe-list";
+import MainNavigation from '../components/layout/main-navigation'; // Import the MainNavigation component
+import RecipeList from './recipe-list/index';
 
-export default function homePage() {
+export default function HomePage() {
   return (
-    // <div>Home Page</div>
-    <RecipeList />
-  )
+    <div>
+      <MainNavigation /> {/* Display the navigation bar */}
+      <RecipeList />
+    </div>
+  );
 }
-
--- pages/recipe-list/index.js
+++ pages/recipe-list/index.js
@@ -1,47 +1,43 @@
 // pages/RecipeList.js
 
 import { connectToDb, getAllRecipes } from "../../utils/mongodb-utils";
+import MainNavigation from "../../components/layout/main-navigation"; // Import your navigation component
 
-export async function getStaticProps () {
-  let client = await connectToDb()
+export async function getStaticProps() {
+  let client = await connectToDb();
 
-          const recipeDocuments = await getAllRecipes(
-              client,
-              'recipes',
-              {_id: -1},
-              1,
-          )
+  const recipeDocuments = await getAllRecipes(
+    client,
+    "recipes",
+    { _id: -1 },
+    1
+  );
 
   return {
     props: {
-      recipes: recipeDocuments
-    }
-  }
-
+      recipes: recipeDocuments,
+    },
+  };
 }
-
 
 export default function RecipeList(props) {
   const { recipes } = props;
-  if ( !recipes ) return <p>Loading...</p>
+  if (!recipes) return <p>Loading...</p>;
   return (
     <div>
+      <MainNavigation /> {/* Include the navigation bar here */}
       <h1>Recipe List</h1>
       <ul>
         {recipes.map((recipe) => (
-          
           <li key={recipe._id}>
             {recipe.images.map((imageUrl, index) => (
-            <img key={index} src={imageUrl} alt={recipe.title} />
-          ))}
+              <img key={index} src={imageUrl} alt={recipe.title} />
+            ))}
             <h2>{recipe.title}</h2>
             <p>{recipe.description}</p>
-            
-           
           </li>
         ))}
       </ul>
     </div>
   );
 }
-
