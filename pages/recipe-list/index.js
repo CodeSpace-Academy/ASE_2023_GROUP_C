06040d5fc0db60aad85688a4f47487b966eceaf6
Kopo Ntheledi <kopontheledi@gmail.com>
2023-10-11 16:18:00 +0200

    added styling

--- pages/recipe-list/index.js
+++ pages/recipe-list/index.js
@@ -1,47 +1,46 @@
 // pages/RecipeList.js
 
+// import Image from "next/image";
 import { connectToDb, getAllRecipes } from "../../utils/mongodb-utils";
 
-export async function getStaticProps () {
-  let client = await connectToDb()
 
-          const recipeDocuments = await getAllRecipes(
-              client,
-              'recipes',
-              {_id: -1},
-              1,
-          )
+export async function getStaticProps() {
+  let client = await connectToDb();
+  const recipeDocuments = await getAllRecipes(
+    client,
+    "recipes",
+    { _id: -1 },
+    1
+  );
 
   return {
     props: {
-      recipes: recipeDocuments
-    }
-  }
-
+      recipes: recipeDocuments,
+    },
+  };
 }
-
 
 export default function RecipeList(props) {
   const { recipes } = props;
-  if ( !recipes ) return <p>Loading...</p>
+
+  if (!recipes) return <p>Loading...</p>;
+
   return (
     <div>
       <h1>Recipe List</h1>
-      <ul>
+      <ul className="flex grid center px-4 py-3">
         {recipes.map((recipe) => (
-          
           <li key={recipe._id}>
-            {recipe.images.map((imageUrl, index) => (
-            <img key={index} src={imageUrl} alt={recipe.title} />
-          ))}
             <h2>{recipe.title}</h2>
+            <img
+              className='w-40 h-40'
+              src={recipe.images[0]}
+              alt={recipe.title}
+            />
             <p>{recipe.description}</p>
-            
-           
           </li>
         ))}
       </ul>
     </div>
   );
 }
-
