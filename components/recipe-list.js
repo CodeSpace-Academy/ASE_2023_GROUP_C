4dcb5a5962410deb68ae66d0bf1458afb39c3786
Kopo Ntheledi <kopontheledi@gmail.com>
2023-10-12 15:42:33 +0200

    adding file to the tailwind.config.js file

--- components/recipe-list.js
+++ components/recipe-list.js
@@ -5,18 +5,17 @@ export default function RecipeList(props) {
   
     return (
       <div>
-        
-        <h1>Recipe List</h1>
-        <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        <h1 className="text-2xl font-bold mb-4">Recipe List</h1>
+        <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4 rounded-lg shadow-md">
           {recipes.map((recipe) => (
-            <li key={recipe._id}>
-              <h2>{recipe.title}</h2>
+            <li key={recipe._id} className="bg-white p-4 rounded-lg shadow-md">
+              <h2 className="text-xl font-semibold">{recipe.title}</h2>
               <img
-                className='w-40 h-40'
+                className="w-40 h-40 object-cover rounded-md"
                 src={recipe.images[0]}
                 alt={recipe.title}
               />
-              <p>{recipe.description}</p>
+              <p className="mt-2">{recipe.description}</p>
             </li>
           ))}
         </ul>
