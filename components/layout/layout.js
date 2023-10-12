cba84ae6cee27c46ad1f684efbad193a481a4460
Kopo Ntheledi <kopontheledi@gmail.com>
2023-10-12 14:52:13 +0200

    created a receipe-list component and exported it to the RecipeCards in pages at index.js

---
+++ components/layout/layout.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import MainNavigation from './main-navigation'
+
+export default function layout() {
+  return (
+    <MainNavigation />
+  )
+}
--- pages/recipe-list/index.js
+++ pages/recipe-list/index.js
@@ -1,48 +1,13 @@
-// pages/RecipeList.js
-
-// import Image from "next/image";
-import { connectToDb, getAllRecipes } from "../../utils/mongodb-utils";
-import MainNavigation from "../../components/layout/main-navigation"; // Import your navigation component
+import MainNavigation from '../../components/layout/main-navigation'
+import RecipeList from '../../components/recipe-list'
 
 
-export async function getStaticProps() {
-  let client = await connectToDb();
-  const recipeDocuments = await getAllRecipes(
-    client,
-    "recipes",
-    { _id: -1 },
-    1
-  );
-
-  return {
-    props: {
-      recipes: recipeDocuments,
-    },
-  };
-}
-
-export default function RecipeList(props) {
-  const { recipes } = props;
-
-  if (!recipes) return <p>Loading...</p>;
-
+function RecipeCards() {
   return (
-    <div>
-      <MainNavigation /> {/* Include the navigation bar here */}
-      <h1>Recipe List</h1>
-      <ul className="flex grid center px-4 py-3">
-        {recipes.map((recipe) => (
-          <li key={recipe._id}>
-            <h2>{recipe.title}</h2>
-            <img
-              className='w-40 h-40'
-              src={recipe.images[0]}
-              alt={recipe.title}
-            />
-            <p>{recipe.description}</p>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+   <div>
+    <RecipeList/>
+   </div>
+  )
 }
+
+export default RecipeCards
