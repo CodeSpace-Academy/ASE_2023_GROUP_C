f2fd8b58437153f6d0e2c4773c3a1dd77872374b
Kutlwano Josi <kutlwanojosi01@gmail.com>
2023-10-12 16:22:15 +0200

    updated index

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import MainNavigation from '../components/layout/main-navigation'; // Import the MainNavigation component
+
 
 export default function HomePage() {
   return (
     <div>
-      <MainNavigation /> {/* Display the navigation bar */}
+
     </div>
   );
 }
