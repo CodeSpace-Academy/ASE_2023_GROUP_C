ffc0a97cd3855eeb62bc3ebecd8d7c32f3c54a70
kwetso12 <kgaladikwetso@gmail.com>
2023-10-13 16:31:35 +0200

    Added details to the Recipe Details

--- components/recipe-cart/recipecard.js
+++ components/recipe-cart/recipecard.js
@@ -1,10 +1,18 @@
 import React from 'react'
-import styles from './instructions.module.css'
+import styles from './recipecart.module.css'
 
-export default function Instructions(prop) {
+export default function RecipeCard(prop) {
     const { recipe } = prop
   return (
     <div>
+      <img
+        src={recipe.images[0]}
+        alt={recipe.title}
+        className={styles.recipeImage}
+      />
+      <h2 className={styles.recipeTitle}>{recipe.title}</h2>
+      <p>Prep Time: {recipe.prep} </p>
+      <p>Cook Time: {recipe.cook} </p>
     {recipe.instructions && recipe.instructions.length > 0 && (
       <div className={styles.instructionsContainer}>
         <h3>Instructions</h3>
