9010d2d38603086137da1933d6e4fa3e51bd9381
keotshepileMaje <keotshepilemaje@gmail.com>
2023-10-18 01:03:54 +0200

    Modified components that handle edits

---
+++ components/recipe-cart/update-recipe/instructions.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import EditRecipeContent from "./editable-text";
+
+function RecipeInstruction(props) {
+  const { recipe, onEdit } = props;
+
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedInstruction, setEditedInstruction] = useState(recipe.instructions);
+
+  const handleEditInstruction = (newInstruction) => {
+
+    setEditedInstruction(
+      newInstruction.split('\n').map(
+        (instruction) => instruction.trim()
+      )
+    ); // Split into an array
+    
+    setIsEditing(false);
+
+    onEdit
+  };
+
+  return (
+    <div>
+      <h3>Instruction</h3>
+      {isEditing ? (
+        <EditRecipeContent
+          initialValue={editedInstruction.join('\n')} // Join the array with line breaks
+          onSave={handleEditInstruction}
+          onCancel={() => setIsEditing(false)}
+        />
+      ) : (
+        <div>
+          <h3>Instructions</h3>
+          <ol>
+            {editedInstruction.map((instruction, index) => (
+              <li key={index}>{instruction}</li>
+            ))}
+          </ol>
+          <button onClick={() => setIsEditing(true)}>Edit Instructions</button>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default RecipeInstruction;
