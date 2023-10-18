b60ed190c87cceb0e3f6621b6bc9ed462c79c367
Kopo Ntheledi <kopontheledi@gmail.com>
2023-10-18 10:45:42 +0200

    updated the recipe-details

--- pages/recipe-list/[recipeDetails].js
+++ pages/recipe-list/[recipeDetails].js
@@ -6,6 +6,7 @@ import {
 } from "../../utils/mongodb-utils";
 import TagsDisplay from "../../components/tags/tags-display";
 import RecipeCard from "../../components/recipe-cart/recipecard";
+import RecipeDescription from "../../components/recipe-description/recipe-description";
 
 export async function getServerSideProps(context) {
   const recipeId = context.query.recipeDetails;
@@ -21,15 +22,19 @@ export async function getServerSideProps(context) {
     };
   }
   let recipeDocuments;
-  
+  let allergens;
 
   try {
     recipeDocuments = await getRecipeDetails(client, "recipes", {
       _id: recipeId,
     });
-    
+    allergens = await getAllergens(client, "allergens");
 
-    return { props: { recipeDocuments } };
+    const allergensList = allergens[0].allergens
+
+    // console.log(allergensList)
+
+    return { props: { recipeDocuments, allergensList } };
   } catch (error) {
     console.error("Getting recipes failed");
     return {
@@ -38,7 +43,21 @@ export async function getServerSideProps(context) {
   }
 }
 
-export default function RecipeDetails({ recipeDocuments}) {
+export default function RecipeDetails({ recipeDocuments, allergensList }) {
+
+  const ingredients = recipeDocuments.ingredients
+  const ingredientsWithAllergensInRecipe = [];
+
+  for (let key in ingredients) {
+    for (let allergen of allergensList) {
+      if (key.toLowerCase().includes(allergen)) {
+        ingredientsWithAllergensInRecipe.push(key);
+        
+      }
+    }
+  }
+
+  console.log(ingredientsWithAllergensInRecipe)
 
   return (
     <div>
