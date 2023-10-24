9177e68b441596069df9fa293bb6950e7bf2f43b
Kopo Ntheledi <kopontheledi@gmail.com>
2023-10-24 10:10:31 +0200

    Fixed the sorting button

--- components/ui-utils/filtering-form.js
+++ components/ui-utils/filtering-form.js
@@ -1,10 +1,24 @@
-export default function SortingOption({ handleSort }) {
-    return (
-      <div>
-        <button onClick={() => handleSort("ascending")}>Ascending</button>
-        <button onClick={() => handleSort("descending")}>Descending</button>
-        <button onClick={() => handleSort("byDate")}>By Date</button>
-        <button onClick={() => handleSort("default")}>Default</button>
-      </div>
-    );
-  }
+export default function SortingOption ({ handleSort })  {
+  return (
+    <div >
+      <button  onClick={() => handleSort("default")}>
+        {/* Default Sort */}
+        Default
+      </button>
+      <button  onClick={() => handleSort("ascending")}>
+        {/* Sort by Prep Time (Ascending) */}
+        Cook Time
+      </button>
+      <button  onClick={() => handleSort("descending")}>
+        {/* Sort by Prep Time (Descending) */}
+        Prep Time
+      </button>
+      <button  onClick={() => handleSort("byDate")}>
+        {/* sort by newest */}
+        Newest
+      </button>
+    </div>
+  );
+};
+
+
