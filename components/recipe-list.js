6688cafbf4ef48817f1207a054cc8ceb94323755
LentsweLetlotloMokotedi <lentsweletlotlo0@gmail.com>
2023-10-24 16:13:35 +0200

    updating recipe list

--- components/recipe-list.js
+++ components/recipe-list.js
@@ -1,22 +1,90 @@
-import React, { useState } from 'react';
-import RecipeInstruction from './update-recipe/instructions';
-import RecipeDescription from './update-recipe/description';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMitten, faUtensils } from '@fortawesome/free-solid-svg-icons';
-import TagsDisplay from '../tags/tags-display';
-import { Carousel } from 'react-responsive-carousel';
-import 'react-responsive-carousel/lib/styles/carousel.min.css'
-import Card from '../ui-utils/card';
+import React, { useState, useEffect } from "react";
+import NoResultsMessage from "./layout/no-results-message";
+import LoadMoreButton from "./ui-utils/load-more-button";
+import Link from "next/link";
+import {
+  faUtensils,
+  faKitchenSet,
+  faHome,
+  faSpoon,
+  faHeart,
+  faSort,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Pagination from "./pagination";
+import SortingOption from "./ui-utils/filtering-form";
 
-export default function RecipeCard(prop) {
-  const { recipe, allergensList } = prop;
+export default function RecipeList(props) {
+  // Destructure props
+  const { recipes: initialRecipes, totalRecipeInDb } = props;
+  const [isFavourate, setIsFavourate] = useState(false);
 
-  const [isEdited, setIsEdited] = useState(false);
+  // State variables
+  const [recipes, setRecipes] = useState(initialRecipes);
+  const [visibleRecipes, setVisibleRecipes] = useState(20);
+  const [remainingRecipes, setRemainingRecipes] = useState(
+    initialRecipes ? Math.max(initialRecipes.length - visibleRecipes, 0) : 0
+  );
+  const [searchInput, setSearchInput] = useState("");
+  const [noResults, setNoResults] = useState(false);
+  const [matchingRecipeCount, setMatchingRecipeCount] = useState(0); // Added state for matching recipe count
 
-  const handleDescriptionEdit = () => {
-    setIsEdited(true);
+  // State for sorting and dropdown visibility
+  const [currentSort, setCurrentSort] = useState("default");
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  // Handle initial recipes and no results
+  useEffect(() => {
+    setRecipes(initialRecipes);
+    updateNoResults(initialRecipes, searchInput);
+  }, [initialRecipes]);
+
+  // Handle search and filtering recipes
+  useEffect(() => {
+    const filteredRecipes = initialRecipes.filter((recipe) =>
+      recipe.title.toLowerCase().includes(searchInput.toLowerCase())
+    );
+
+    setRecipes(filteredRecipes);
+    const newVisibleRecipes = Math.min(visibleRecipes, filteredRecipes.length);
+    setRemainingRecipes(
+      Math.max(filteredRecipes.length - newVisibleRecipes, 0)
+    );
+
+    // Update the count of matching recipes
+    setMatchingRecipeCount(filteredRecipes.length);
+
+    updateNoResults(filteredRecipes, searchInput);
+  }, [searchInput, visibleRecipes, initialRecipes]);
+
+  // Function to update 'noResults' state
+  const updateNoResults = (filteredRecipes, input) => {
+    setNoResults(filteredRecipes.length === 0 && input.trim() !== "");
   };
 
+  // Function to handle sorting
+  const handleSort = (option) => {
+    setCurrentSort(option);
+    let sortedRecipes = [...initialRecipes]; // Use the initial recipes for sorting
+
+    setRecipes(sortedRecipes);
+    setIsDropdownOpen(false); // Close the dropdown after selecting an option
+  };
+
+  // Function to toggle the sorting options dropdown
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+// Function to load more recipes
+const loadMore = () => {
+  const additionalRecipes = 20;
+  const newVisibleRecipes = visibleRecipes + additionalRecipes;
+  setVisibleRecipes(newVisibleRecipes);
+  setRemainingRecipes(Math.max(recipes.length - newVisibleRecipes, 0)); // Add a closing parenthesis here
+};
+
+  // Function to convert minutes to hours and minutes
   const convertToHours = (minutes) => {
     if (minutes >= 60) {
       const hours = Math.floor(minutes / 60);
@@ -26,73 +94,118 @@ export default function RecipeCard(prop) {
     return `${minutes} minutes`;
   };
 
+  // Handle search when the user clicks the "Search" button
+  const handleSearch = () => {
+    const filteredRecipes = initialRecipes.filter((recipe) =>
+      recipe.title.toLowerCase().includes(searchInput.toLowerCase())
+    );
+
+    setRecipes(filteredRecipes);
+    const newVisibleRecipes = Math.min(visibleRecipes, filteredRecipes.length);
+    setRemainingRecipes(Math.max(filteredRecipes.length - newVisibleRecipes, 0));
+
+    // Update the count of matching recipes
+    setMatchingRecipeCount(filteredRecipes.length);
+
+    updateNoResults(filteredRecipes, searchInput);
+  };
+
   return (
-    <div>
-
-      {/* {isEdited && <p>Recipe was edited</p>} */}
-    <div className='p-4 flex flex-col  gap-3 bg-slate-900 text-red-100 justify-center'>
-    <div className='p-4 flex flex-col md:flex-row md:gap-10 bg-slate-900 text-red-100 '>
-      <Carousel 
-        className=' md:max-w-2xl md:border-r-2 md:border-gray-400 md:pr-5'
-        showArrows={true}
-        >
-          {recipe.images.map((image, index) =>(
-              <div key = {index} className=' max-h-80'>
-                <img
-                  src={image}
-                  alt={recipe.title}
-                  className=""
-                  />
+    <div className="bg-gray-900 text-white h-screen p-4 flex flex-col">
+      <div className="flex items-center">
+        <Link href="/">
+          <FontAwesomeIcon icon={faHome} size="lg" className="p-2" />
+        </Link>
+        <div className="relative inline-block text-white">
+          <div className="sorting-container relative">
+            <FontAwesomeIcon icon={faSort} size="lg" onClick={toggleDropdown} />
+            {isDropdownOpen && (
+              <div className="z-10">
+                <SortingOption handleSort={handleSort} />
               </div>
-          ))}
-        </Carousel>
-
-        <Card className=" md:w-96">
-      <h2 
-        className= 'text-3xl font-bold '
-      >
-        {recipe.title}
-        </h2>
-        <div className='flex mt-4 space-x-4 justify-center gap-8 mb-2'>
-       <p><FontAwesomeIcon icon={faUtensils} size='xl'/> Prep: {convertToHours(recipe.prep)} </p>
-       <p><FontAwesomeIcon icon={faMitten} size='xl'/> Cook: {convertToHours(recipe.cook)} </p>
-      </div>
-        <TagsDisplay recipe={recipe} />
-        <RecipeDescription recipe={recipe} allergensList={allergensList} onEdit={handleDescriptionEdit} />
-            </Card>
-      
-    </div>
-
-      <Card>
-      {recipe.ingredients && Object.keys(recipe.ingredients).length > 0 && (
-        <div>
-          <h3 className="text-2xl font-semibold pb-2 pt-2 ">Ingredients:</h3>
-          <ul>
-            {Object.keys(recipe.ingredients).map((ingredientKey, index) => (
-              <li key={index}> { 
-                recipe.ingredients[ingredientKey]} of {ingredientKey} </li>)
             )}
-          </ul>
-        </div>
-      )}
-      </Card>
-
-      <Card>
-    {recipe.instructions && recipe.instructions.length > 0 && (
-      <div className=''>
-
-  
-        {recipe.instructions && recipe.instructions.length > 0 && (
-          <div className="">
-            <h3 className="text-2xl font-semibold pb-2 pt-2">Instructions</h3>
-            <RecipeInstruction recipe={recipe} onEdit={handleDescriptionEdit} />
           </div>
+        </div>
+      </div>
+
+      <div className="search-bar-container flex items-center mb-4">
+        <input
+          type="text"
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
+          placeholder="Search..."
+          className="w-3/4 p-2 border rounded-l text-black"
+        />
+        <button
+          onClick={handleSearch}
+          className="bg-blue-700 text-white p-2 rounded-r hover:bg-blue-800"
+        >
+          Search
+        </button>
+        <Link href="/favourite-recipes">
+          <button className="text-white p-2">Favorite Recipes</button>
+        </Link>
+      </div>
+      {noResults && <NoResultsMessage />}
+
+      <div className="matching-recipe-count">
+        {matchingRecipeCount > 0 && (
+          <p>{matchingRecipeCount} matching recipes found</p>
         )}
       </div>
-    )}
-    </Card>
-        </div>
-        
-        </div>
-  )
+
+      <div className="recipe-list-container overflow-y-auto flex-grow">
+        <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          {recipes.slice(0, visibleRecipes).map((recipe) => (
+            <li key={recipe._id}>
+              <div className="relative bg-gray-800 p-4 rounded-lg transition hover:shadow-lg flex flex-col flex-wrap w-200">
+                <img
+                  src={recipe.images[0]}
+                  alt={recipe.title}
+                  className="w-full h-48 object-cover rounded-md"
+                />
+                <button className="absolute right-4 m-3 rounded-full w-14 text-center">
+                  {isFavourate ? (
+                    <FontAwesomeIcon icon={faHeart} />
+                  ) : (
+                    <FontAwesomeIcon icon={faHeart} />
+                  )}
+                </button>
+
+                <div className="flex justify-between">
+                  <h2 className="text-xl font-semibold mt-2">{recipe.title}</h2>
+                </div>
+                <p className="mt-2">
+                  <FontAwesomeIcon icon={faUtensils} /> Prep:{" "}
+                  {convertToHours(recipe.prep)}{" "}
+                </p>
+                <p>
+                  <FontAwesomeIcon icon={faKitchenSet} /> Cook:{" "}
+                  {convertToHours(recipe.cook)}{" "}
+                </p>
+                <p>
+                  <FontAwesomeIcon icon={faSpoon} /> Total:{" "}
+                  {convertToHours(recipe.prep + recipe.cook)}{" "}
+                </p>
+
+                <Link href={`/recipe-details/${recipe._id}`} className="mt-4">
+                  <button>View Recipe</button>
+                </Link>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="bg-gray-900 p-4 flex justify-center items-center">
+        {remainingRecipes > 0 && (
+          <LoadMoreButton
+            onClick={loadMore}
+            remainingRecipes={remainingRecipes}
+            className="bg-blue-700 text-white px-2 py-1 rounded-full hover-bg-blue-800"
+          />
+        )}
+      </div>
+      <Pagination totalRecipeInDb={totalRecipeInDb} />
+    </div>
+  );
 }
