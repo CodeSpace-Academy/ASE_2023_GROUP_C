c1c7aaf247931dc720a64c81b22505ccdac950d4
LentsweLetlotloMokotedi <lentsweletlotlo0@gmail.com>
2023-10-25 13:56:33 +0200

    Added favourite Api route

---
+++ pages/api/favourite.js
@@ -0,0 +1,29 @@
+import { connectToDb, getAllRecipes } from "../../../utils/mongodb-utils";
+import { insertDocument } from "../../utils/mongodb-utils";
+
+export default async function handler(req, res) {
+    let client;
+    
+    try {
+      client = await connectToDb()
+    } catch(error) {
+    res.status(500).json({message: 'Database connection failed'})
+    return 
+  }
+  
+  const document = {
+
+  }
+  if (req.method === 'POST') {
+    try {
+      const recipeDocuments = await insertDocument(
+        client,
+        'users-list',
+        document,
+      )
+      res.status(201).json({message: recipeDocuments})
+    } catch (error) {
+      res.status(500).json({message: 'Getting recipes failed'})
+    }
+  }
+}
