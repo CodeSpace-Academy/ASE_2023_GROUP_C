4c132ebc84e23cf17e30d8b4046d937d2bcc69a5
Kopo Ntheledi <kopontheledi@gmail.com>
2023-10-25 14:13:08 +0200

    updates

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import Link from 'next/link';
-import RecipeHomePage from '../components/home-page/home-page';
+import RecipeHomePage from '../components/homePage/homePage';
 import { useUser } from '@auth0/nextjs-auth0/client';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 export default function HomePage() {
-  const { user, error, isLoading } = useUser();
-
-  if (isLoading)
-    return (
-      <div className=" min-h-screen items-center justify-center bg-slate-900 flex">
-        <FontAwesomeIcon icon={faSpinner} spinPulse />
-      </div>
-    );
+ 
   const { user, error, isLoading } = useUser();
 
   if (isLoading)
