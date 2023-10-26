43e7cb3f2713cee184983e7a60c9b28a30984e9f
Lmethikge <l.ematshego12@gmail.com>
2023-10-26 16:15:01 +0200

    fixed the code

--- components/custom-prompt/prompt.js
+++ components/custom-prompt/prompt.js
@@ -10,21 +10,23 @@ const CustomPrompt = ({ isOpen, onClose, onConfirm }) => {
     const handleConfirm = () => {
       onConfirm(inputValue);
      onClose();
-    };
+   };
 
  // A div element with a class name 'custom-prompt' and conditional 'open' class based on the value of isOpen.
   return (
    <div className={`custom-prompt ${isOpen ? 'open' : ''}`}>
-      <div className="modal-content text-center">
-        <p className='text-white'> Are you sure you want to unfavourite ?</p>
+     <div className="modal-content text-center">
+       <p className='text-white'> Are you sure you want to unfavourite ?</p>
        <button onClick={handleConfirm}>OK</button>
-       <button onClick={onClose}>Cancel</button>
+      <button onClick={onClose}>Cancel</button>
       </div>
     </div>
  );
-  };
+ };
   
 
 export default CustomPrompt;
+
+
 
 
--- pages/favouriteRecipes.js
+++ pages/favouriteRecipes.js
@@ -1,5 +1,16 @@
 import FavouriteRecipes from "../components/favourite/favouriteRecipes";
+import NoFavouritesYet from '../components/favourite/no-favourites';
+import CustomPrompt from '../components/custom-prompt/prompt';
 
 export default function Favorites() {
-  return <FavouriteRecipes />;
+  return(
+    <div>
+  
+  <FavouriteRecipes />
+  <NoFavouritesYet />
+  <CustomPrompt />
+    </div>
+  )
 }
+
+
