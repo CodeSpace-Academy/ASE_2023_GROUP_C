700044d6c5f5a4f76a3c326e1019b42fe8c98a2e
Lmethikge <l.ematshego12@gmail.com>
2023-10-27 14:11:55 +0200

    Added home and favourite button

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,12 +1,23 @@
 import { Fragment } from 'react'
 import SearchBar from './searchBar'
+import Link from 'next/link'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome } from '@fortawesome/free-solid-svg-icons'
 
 
 export default function Layout(props) {
   return (
     <Fragment>
-      <div className=' bg-slate-900 '>
+      <div >
+      <Link href="/">
+          <FontAwesomeIcon icon={faHome} size="lg" className="p-2" />
+        </Link>
       <SearchBar />
+      <div className="search-bar-container items-center mb-4">
+        <Link href="/favouriteRecipes">
+          <button className="text-white p-2">Favorite Recipes</button>
+        </Link>
+      </div>
       {props.children}
       </div>
     </Fragment>
