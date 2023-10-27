192e98351724f96c418bd3b86dc95f7934545c85
keotshepileMaje <keotshepilemaje@gmail.com>
2023-10-27 16:52:56 +0200

    Added conditions

--- pages/api/favourite.js
+++ pages/api/favourite.js
@@ -1,25 +1,41 @@
-import { updateUsersList } from "../../utils/mongodb-utils";
+import { insertDocument, lookforDocument, updateUsersList } from "../../utils/mongodb-utils";
 
 export default async function handler(req, res) {
   const recipe = req.body
-  const username = 'The User 1'
-
-  const favouredRecipe = {
-    userName: 'The User 1',
-    userList: []
-  }
+  const username = 'The User 1'  // To-Do: make this a dynamic value of a user. This value must nbe set according to how the user is.
 
   if (req.method === 'POST') {
-    try {
-      const recipeDocuments = await updateUsersList(
-        'users-list',
-        username,
-        recipe
-      )
 
-      res.status(201).json({message: recipe})
-    } catch (error) {
-      res.status(500).json({message: 'Getting recipes failed'})
+    const userExist = await lookforDocument({'userName': username})
+
+    if (userExist) {
+      try {
+        const recipeDocuments = await updateUsersList(
+          'users-list',
+          username,
+          recipe
+        )
+        res.status(201).json({message: recipe})
+      } catch (error) {
+        res.status(500).json({message: 'Getting recipes failed'})
+      }
+    } else {
+      const favouredRecipe = {
+        userName: username,
+        userList: [recipe]
+      }
+    
+      try {
+        const recipeDocuments = await insertDocument(
+          'users-list',
+          favouredRecipe,
+        )
+        favouredRecipe._id = recipeDocuments.insertedId
+        res.status(201).json({message: favouredRecipe})
+      } catch (error) {
+        res.status(500).json({message: 'Getting recipes failed'})
+      }
+    
     }
   }
 }
