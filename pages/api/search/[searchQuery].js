6c79802fbb95c06ca8b4e9821efe84a19162634a
Kutlwano Josi <kutlwanojosi01@gmail.com>
2023-10-27 11:16:32 +0200

    Added Documentation

--- pages/api/search/[searchQuery].js
+++ pages/api/search/[searchQuery].js
@@ -1,77 +1,25 @@
-// const DUMMYDATA = {
-    
-//         "_id": "6b432ef8-a563-4eef-9f31-827987a3e0c9",
-//         "title": "Butter Pecan Cookies",
-//         "description": "Make and share this Butter Pecan Cookies recipe from Food.com.",
-//         "prep": 55,
-//         "cook": 9,
-//         "category": "Dessert",
-//         "servings": "",
-//         "published": "1999-09-07T00:00:00.000Z",
-//         "tags": [
-//           "Cookie & Brownie",
-//           "Fruit",
-//           "Nuts",
-//           "Weeknight",
-//           "Oven",
-//           "< 4 Hours"
-//         ],
-//         "ingredients": {
-//           "butter": "3/4",
-//           "brown sugar": "1/2",
-//           "granulated sugar": "1",
-//           "vanilla extract": "1",
-//           "flour": "1",
-//           "pecan halves": "2"
-//         },
-//         "images": [
-//           "https://img.sndimg.com/food/image/upload/w_555,h_416,c_fit,fl_progressive,q_95/v1/img/recipes/47/picfnmxck.jpg",
-//           "https://img.sndimg.com/food/image/upload/w_555,h_416,c_fit,fl_progressive,q_95/v1/img/recipes/47/picCPvxZU.jpg"
-//         ],
-//         "instructions": [
-//           "Preheat oven to 350 degrees.",
-//           "Cream butter in large mixing bowl.",
-//           "Gradually add brown sugar and granulated sugar.",
-//           "Cream well.",
-//           "Add unbeaten egg yolk and vanilla and beat well.",
-//           "Blend in sifted flour to form a stiff dough.",
-//           "Shape dough into small balls.",
-//           "Place on greased cookie sheet. Flatten cookies with bottom of glass dipped in sugar.",
-//           "Bake at 350 degrees for 7-9 minutes, till golden brown (do not overbrown.) Cool before frosting.",
-//           "Garnish with pecan halves."
-//         ],
-//         "nutrition": {
-//           "calories": "69",
-//           "fat": "5.6",
-//           "saturated": "1.4",
-//           "sodium": "15",
-//           "carbohydrates": "4.5",
-//           "fiber": "0.6",
-//           "sugar": "1.6",
-//           "protein": "0.8"
-//         }
-//       }
 import { getAllRecipes } from "../../../utils/mongodb-utils";
 
-
-
 export default async function handler(req, res) {
-    
-        
-    if ( req.method === 'GET') {
-        // const userQuery = req.query.searchQuery === 'all' ? '': req.query.searchQuery; // User's search query
-        const userQuery = req.query.searchQuery  // User's search query
-        const regexPattern = new RegExp(`.*${userQuery}.*`, "i")
-    
-        const recipes =  await getAllRecipes(
+    // Check if the HTTP method is GET
+    if (req.method === 'GET') {
+        // Extract the user's search query from the request's query parameters
+        const userQuery = req.query.searchQuery;
 
-            'recipes',
-            {_id: -1},
-            2,
+        // Create a regular expression pattern for a case-insensitive search
+        const regexPattern = new RegExp(`.*${userQuery}.*`, "i");
+
+        // Fetch recipes from the database that match the search query
+        const recipes = await getAllRecipes(
+            'recipes',  // Collection name
+            { _id: -1 }, // Sort by _id in descending order
+            2, // Limit the number of results to 2
             {
-                title: { $regex: regexPattern }
+                title: { $regex: regexPattern } // Match recipes with titles that contain the search query
             }
         );
-        res.status(200).json({message: recipes })
+
+        // Respond with a 200 status and the fetched recipes as a JSON object
+        res.status(200).json({ message: recipes });
     }
 }
--- pages/search/[searchQuery].js
+++ pages/search/[searchQuery].js
@@ -1,27 +1,32 @@
-import { useEffect, useState} from 'react'
-import { useRouter } from 'next/router'
-import RecipeList from '../../components/recipeList/recipeList'
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import RecipeList from "../../components/recipeList/recipeList";
 
 export default function SearchResultPage(props) {
+  // Get the router instance from Next.js
+  const router = useRouter();
 
-   const router = useRouter()
-   const searchQuery = router.query.searchQuery
-   const [ searchResult, setSearchResult ] = useState('')
+  // Extract the searchQuery from the router's query parameters
+  const searchQuery = router.query.searchQuery;
 
-   useEffect(() => {
+  // Initialize the searchResult state with an empty string
+  const [searchResult, setSearchResult] = useState("");
+
+  // Use the useEffect hook to fetch search results when the searchQuery changes
+  useEffect(() => {
+    // Fetch search results from the server's API
     fetch(`/api/search/${searchQuery}`)
-    .then(res => res.json())
-    .then(data => setSearchResult(data.message) )
-   },[searchQuery]
-   ) 
+      .then((res) => res.json())
+      .then((data) => setSearchResult(data.message));
+  }, [searchQuery]);
 
-   if (!searchResult) return <h1>Loading...</h1>
+  // If searchResult is not available yet, display a loading message
+  if (!searchResult) return <h1>Loading...</h1>;
 
-
-
+  // Render the RecipeList component with the search results
   return (
     <div>
       <RecipeList recipes={searchResult} totalRecipeInDb={0} />
     </div>
-  )
+  );
 }
