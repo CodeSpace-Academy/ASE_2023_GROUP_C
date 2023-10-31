26f3e08ed3ab9c2dd693262658364f0ef4c9c13a
Kutlwano Josi <kutlwanojosi01@gmail.com>
2023-10-31 13:38:24 +0200

    added css and comments

--- components/recipeList/recipeList.js
+++ components/recipeList/recipeList.js
@@ -39,6 +39,7 @@ export default function RecipeList(props) {
    * @param {string} option - The selected sorting option.
    */
   const handleSort = (option) => {
+    // Update the current sorting option
     setCurrentSort(option);
     let sortedRecipes = [...recipes]; // Use the current state of recipes for sorting
 
@@ -72,16 +73,18 @@ export default function RecipeList(props) {
         break;
     }
 
+    // Update the state with the sorted recipes
     setRecipes(sortedRecipes);
     setIsDropdownOpen(false);
-  }
+  };
 
   /**
    * Toggles the sorting options dropdown.
    */
   const toggleDropdown = () => {
+    // Toggle the visibility of the sorting dropdown
     setIsDropdownOpen(!isDropdownOpen);
-  }
+  };
 
   /**
    * Loads more recipes when the "Load More" button is clicked.
@@ -89,9 +92,10 @@ export default function RecipeList(props) {
   const loadMore = () => {
     const additionalRecipes = 20;
     const newVisibleRecipes = visibleRecipes + additionalRecipes;
+    // Update the state with the new visible and remaining recipes
     setVisibleRecipes(newVisibleRecipes);
     setRemainingRecipes(Math.max(recipes.length - newVisibleRecipes, 0));
-  }
+  };
 
   /**
    * Converts minutes to hours and minutes format.
@@ -105,74 +109,90 @@ export default function RecipeList(props) {
       return `${hours} hours ${remainingMinutes} minutes`;
     }
     return `${minutes} minutes`;
-  }
+  };
 
   return (
-    <div className="bg-gray-900 text-white h-screen flex">
-      <div className="w-16 p-4">
+    <>
+      <div className="w-16">
         <NavBar />
       </div>
-      <div className="flex-1 p-4">
-        <div className="relative inline-block text-white">
-          <div className="sorting-container relative">
-            <FontAwesomeIcon icon={faSort} size="lg" onClick={toggleDropdown} />
-            {isDropdownOpen && (
-              <div className="z-10">
-                <SortingOption handleSort={handleSort} />
-              </div>
+      <div className="bg-gray-900 text-white h-screen flex">
+        <div className="flex-1 p-4">
+          <div className="relative inline-block text-white">
+            <div className="sorting-container relative">
+              {/* Sorting dropdown icon */}
+              <FontAwesomeIcon
+                icon={faSort}
+                size="lg"
+                onClick={toggleDropdown}
+              />
+              {isDropdownOpen && (
+                // Display the sorting options when the dropdown is open
+                <div className="z-10">
+                  <SortingOption handleSort={handleSort} />
+                </div>
+              )}
+            </div>
+          </div>
+          <div
+            className="recipe-list-container overflow-y-auto flex-grow"
+            style={{ backgroundColor: "rgb(16, 23, 42)" }}
+          >
+            <ul className=" mt-20 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+              {recipes.slice(0, visibleRecipes).map((recipe) => (
+                <li key={recipe._id}>
+                  <div className="relative bg-gray-800 p-4 rounded-lg transition hover:shadow-lg flex flex-col flex-wrap w-200">
+                    {/* Recipe image */}
+                    <img
+                      src={recipe.images[0]}
+                      alt={recipe.title}
+                      className="w-full h-48 object-cover rounded-md"
+                    />
+                    {/* Favorite button for the recipe */}
+                    <FavoriteButton recipe={recipe} />
+                    <div className="flex justify between">
+                      <h2 className="text-xl font-semibold mt-2">
+                        {recipe.title}
+                      </h2>
+                    </div>
+                    <p className="mt-2">
+                      <FontAwesomeIcon icon={faUtensils} /> Prep:{" "}
+                      {convertToHours(recipe.prep)}{" "}
+                    </p>
+                    <p>
+                      <FontAwesomeIcon icon={faKitchenSet} /> Cook:{" "}
+                      {convertToHours(recipe.cook)}
+                    </p>
+                    <p>
+                      <FontAwesomeIcon icon={faSpoon} /> Total:{" "}
+                      {convertToHours(recipe.prep + recipe.cook)}
+                    </p>
+                    <Link
+                      href={`/recipeDetails/${recipe._id}`}
+                      className="mt-4"
+                    >
+                      {/* Link to view the recipe details */}
+                      <button>View Recipe</button>
+                    </Link>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="bg-gray-900 p-4 flex justify-center items-center">
+            {remainingRecipes > 0 && (
+              // Display the "Load More" button when there are remaining recipes
+              <LoadMoreButton
+                onClick={loadMore}
+                remainingRecipes={remainingRecipes}
+                className="bg-blue-700 text-white px-2 py-1 rounded-full hover-bg-blue-800"
+              />
             )}
           </div>
+          {/* Pagination component to navigate through the recipe list */}
+          <Pagination totalRecipeInDb={totalRecipeInDb} />
         </div>
-        <div
-          className="recipe-list-container overflow-y-auto flex-grow"
-          style={{ backgroundColor: "rgb(16, 23, 42)" }}
-        >
-          <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-            {recipes.slice(0, visibleRecipes).map((recipe) => (
-              <li key={recipe._id}>
-                <div className="relative bg-gray-800 p-4 rounded-lg transition hover:shadow-lg flex flex-col flex-wrap w-200">
-                  <img
-                    src={recipe.images[0]}
-                    alt={recipe.title}
-                    className="w-full h-48 object-cover rounded-md"
-                  />
-                  <FavoriteButton recipe={recipe} />
-                  <div className="flex justify-between">
-                    <h2 className="text-xl font-semibold mt-2">
-                      {recipe.title}
-                    </h2>
-                  </div>
-                  <p className="mt-2">
-                    <FontAwesomeIcon icon={faUtensils} /> Prep:{" "}
-                    {convertToHours(recipe.prep)}{" "}
-                  </p>
-                  <p>
-                    <FontAwesomeIcon icon={faKitchenSet} /> Cook:{" "}
-                    {convertToHours(recipe.cook)}
-                  </p>
-                  <p>
-                    <FontAwesomeIcon icon={faSpoon} /> Total:{" "}
-                    {convertToHours(recipe.prep + recipe.cook)}
-                  </p>
-                  <Link href={`/recipeDetails/${recipe._id}`} className="mt-4">
-                    <button>View Recipe</button>
-                  </Link>
-                </div>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className="bg-gray-900 p-4 flex justify-center items-center">
-          {remainingRecipes > 0 && (
-            <LoadMoreButton
-              onClick={loadMore}
-              remainingRecipes={remainingRecipes}
-              className="bg-blue-700 text-white px-2 py-1 rounded-full hover-bg-blue-800"
-            />
-          )}
-        </div>
-        <Pagination totalRecipeInDb={totalRecipeInDb} />
       </div>
-    </div>
+    </>
   );
 }
