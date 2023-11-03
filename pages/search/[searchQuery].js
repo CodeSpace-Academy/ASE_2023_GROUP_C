3bf9c3800fbaf0020100b1dfabb1afadb9b49089
Kealeboga Letlhake <letlhake.kv@gmail.com>
2023-11-03 10:45:06 +0200

    Update [searchQuery].js
    Display The Number Recipes That Match The Search Terms

--- pages/search/[searchQuery].js
+++ pages/search/[searchQuery].js
@@ -4,53 +4,50 @@ import RecipeList from "../../components/recipeList/recipeList";
 import { getAllRecipes } from "../../utils/mongodb-utils";
 import NoRecipeMessage from "../noRecipeMessage";
 
-
 export default function SearchResultPage(props) {
-  const router = useRouter()
-  const searchResult = props.recipes
-  const query = router.query.searchQuery
+  const router = useRouter();
+  const searchResult = props.recipes;
+  const query = router.query.searchQuery;
+  const count = searchResult.length; // Number of matching recipes
 
-  if (searchResult.length === 0){
-    return <NoRecipeMessage />
+  if (count === 0) {
+    return <NoRecipeMessage />;
   }
 
   return (
     <div>
-      <h1 className="text-center text-5xl font-bold text-white">SHOWING RESULTS FOR "{query}"</h1>
-      <RecipeList recipes={searchResult} totalRecipeInDb={0} searchQuery = {query} />
+      <h1 className="text-center text-5xl font-bold text-white">
+        SHOWING RESULTS FOR "{query}" ({count} recipes found)
+      </h1>
+      <RecipeList recipes={searchResult} totalRecipeInDb={0} searchQuery={query} />
     </div>
   );
 }
 
 export async function getServerSideProps(context) {
-  const searchQuery = context.params?.searchQuery
-  //regular expression for search mongoDB 
-  const regexPattern = new RegExp(`.*${searchQuery}.*`,"i")
+  const searchQuery = context.params?.searchQuery;
+  // Regular expression for searching MongoDB
+  const regexPattern = new RegExp(`.*${searchQuery}.*`, "i");
 
-  let recipes
+  let recipes;
 
   try {
-     recipes = await getAllRecipes(
-      'recipes',  // Collection name
+    recipes = await getAllRecipes(
+      "recipes", // Collection name
       { _id: -1 }, // Sort by _id in descending order
       2, // Limit the number of results to 2
       {
-          title: { $regex: regexPattern }, // Match recipes with titles that contain the search query
-          
+        title: { $regex: regexPattern }, // Match recipes with titles that contain the search query
       }
-  );
-  if (!recipes) throw new Error('No recipe found!');
-  } catch(error){
-    console.log('Error getting all recipes')
+    );
+    if (!recipes) throw new Error("No recipe found!");
+  } catch (error) {
+    console.log("Error getting all recipes");
   }
 
-  return{
-    props:{
-      recipes:[
-        ...recipes
-    ]
-  }
+  return {
+    props: {
+      recipes: [...recipes],
+    },
+  };
 }
-}
-
-
