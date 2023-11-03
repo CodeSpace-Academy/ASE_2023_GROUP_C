eaea6a1869993437eef3d1f433210c66925cca75
Lmethikge <l.ematshego12@gmail.com>
2023-11-03 09:54:08 +0200

    changed the text color

--- components/favourite/noFavourites.js
+++ components/favourite/noFavourites.js
@@ -1,6 +1,6 @@
 export  default function NoFavouritesYet() {
     return (
-        <div>   
+        <div className='text-white flex  justify-center '>   
             <p>You haven't favourited any recipes yet.</p>  
     </div>
     );
