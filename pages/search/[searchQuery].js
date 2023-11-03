b06c03d5fef336f9b4f2cb9750095fa8e3f77864
Kealeboga Letlhake <letlhake.kv@gmail.com>
2023-11-03 11:13:24 +0200

    Update [searchQuery].js
    Added documentation?

--- pages/search/[searchQuery].js
+++ pages/search/[searchQuery].js
@@ -6,14 +6,16 @@ import NoRecipeMessage from "../noRecipeMessage";
 
 export default function SearchResultPage(props) {
   const router = useRouter();
-  const searchResult = props.recipes;
-  const query = router.query.searchQuery;
-  const count = searchResult.length; // Number of matching recipes
+  const searchResult = props.recipes; // Store the search result recipes received as a prop
+  const query = router.query.searchQuery; // Extract the search query from the router's query parameters
+  const count = searchResult.length; // Calculate the number of matching recipes
 
+  // If there are no matching recipes, display a message
   if (count === 0) {
     return <NoRecipeMessage />;
   }
 
+  // If there are matching recipes, display them along with the search query and count
   return (
     <div>
       <h1 className="text-center text-5xl font-bold text-white">
@@ -24,14 +26,17 @@ export default function SearchResultPage(props) {
   );
 }
 
+// This function runs on the server side and fetches matching recipes from a MongoDB database.
 export async function getServerSideProps(context) {
-  const searchQuery = context.params?.searchQuery;
-  // Regular expression for searching MongoDB
+  const searchQuery = context.params?.searchQuery; // Extract the search query from the URL parameters
+
+  // Create a regular expression pattern for searching MongoDB, case-insensitive
   const regexPattern = new RegExp(`.*${searchQuery}.*`, "i");
 
   let recipes;
 
   try {
+    // Call a function to get matching recipes from the MongoDB database
     recipes = await getAllRecipes(
       "recipes", // Collection name
       { _id: -1 }, // Sort by _id in descending order
@@ -40,11 +45,14 @@ export async function getServerSideProps(context) {
         title: { $regex: regexPattern }, // Match recipes with titles that contain the search query
       }
     );
+
+    // If no recipes are found, throw an error
     if (!recipes) throw new Error("No recipe found!");
   } catch (error) {
     console.log("Error getting all recipes");
   }
 
+  // Return the matching recipes as props to the SearchResultPage component
   return {
     props: {
       recipes: [...recipes],
