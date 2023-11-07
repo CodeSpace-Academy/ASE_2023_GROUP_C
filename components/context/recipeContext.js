fd79d3d3221f0c25da8f7288c7e61deeca5174e5
keotshepileMaje <keotshepilemaje@gmail.com>
2023-11-07 11:57:56 +0200

    Added documentation

--- components/context/recipeContext.js
+++ components/context/recipeContext.js
@@ -1,18 +1,34 @@
 import { Children, createContext, useState } from "react";
 
-export const FilterContext = createContext()
+/**
+ * Context for managing recipe filtering.
+ * @type {React.Context<{ filter: object, setFilter: Function }>}
+ */
+export const FilterContext = createContext();
 
-export function RecipeProvider({children}) {
+/**
+ * Provides a context to manage recipe filtering for child components.
+ *
+ * @param {object} props - Component props.
+ * @param {React.ReactNode} props.children - Child components to be wrapped by the context provider.
+ * @returns {JSX.Element} - JSX element containing the context provider.
+ */
+
+export function RecipeProvider({ children }) {
+    // Define initial state for the filter object using useState.
     const [filter, setFilter] = useState({
-        categories: '', 
-        numberOfSteps: '', 
+        categories: "",
+        numberOfSteps: "",
+        filterByIngredients: ""
     });
 
-    const valueObject = { filter , setFilter }
+    // Create a value object that contains the filter state and the setFilter function.
+    const valueObject = { filter, setFilter };
 
     return (
+        // Provide the FilterContext with the value object to its children.
         <FilterContext.Provider value={valueObject}>
             {children}
         </FilterContext.Provider>
-    )
+    );
 }
