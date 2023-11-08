a68d21b666da44460b9d7ef71c2e7951034f1e1c
keotshepileMaje <keotshepilemaje@gmail.com>
2023-11-08 15:04:21 +0200

    Revert "Added list of tags and installed eslint"
    This reverts commit ab81dd17e877be222607365b4e1709e1c3fbe8b1.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
-
-module.exports = (phase) => {
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
+ 
+module.exports = (phase, { defaultConfig }) => {
   if (phase === PHASE_DEVELOPMENT_SERVER) {
     return {
       reactStrictMode: true,
@@ -10,30 +10,17 @@ module.exports = (phase) => {
         MONGODB_CLUSTERNAME: 'groupc',
         MONGODB_DATABASE: 'dev-dummy',
       },
-    };
+    }
   }
 
+
   return {
-    reactStrictMode: true,
-    env: {
-      MONGODB_USERNAME: 'groupc',
-      MONGODB_PASSWORD: 'UFX6A2FjZLMhDuME',
-      MONGODB_CLUSTERNAME: 'groupc',
-      MONGODB_DATABASE: 'devdb',
-    },
-  };
-};
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-
-  images: {
-    formats: ['image/avif', 'image/webp'],
-    domains: ['img.sndimg.com', 'images.pexels.com'], // Updated from remotePatterns to domains
+      reactStrictMode: true,
+  env: {
+    MONGODB_USERNAME: 'groupc',
+    MONGODB_PASSWORD: 'UFX6A2FjZLMhDuME',
+    MONGODB_CLUSTERNAME: 'groupc',
+    MONGODB_DATABASE: 'devdb',
   },
-  eslint: {
-    dirs: ['pages/tags'],
-  },
-
-};
+  }
+}
