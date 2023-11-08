ab81dd17e877be222607365b4e1709e1c3fbe8b1
keotshepileMaje <keotshepilemaje@gmail.com>
2023-11-08 13:50:58 +0200

    Added list of tags and installed eslint

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
- 
-module.exports = (phase, { defaultConfig }) => {
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
+
+module.exports = (phase) => {
   if (phase === PHASE_DEVELOPMENT_SERVER) {
     return {
       reactStrictMode: true,
@@ -10,17 +10,30 @@ module.exports = (phase, { defaultConfig }) => {
         MONGODB_CLUSTERNAME: 'groupc',
         MONGODB_DATABASE: 'dev-dummy',
       },
-    }
+    };
   }
-
 
   return {
-      reactStrictMode: true,
-  env: {
-    MONGODB_USERNAME: 'groupc',
-    MONGODB_PASSWORD: 'UFX6A2FjZLMhDuME',
-    MONGODB_CLUSTERNAME: 'groupc',
-    MONGODB_DATABASE: 'devdb',
+    reactStrictMode: true,
+    env: {
+      MONGODB_USERNAME: 'groupc',
+      MONGODB_PASSWORD: 'UFX6A2FjZLMhDuME',
+      MONGODB_CLUSTERNAME: 'groupc',
+      MONGODB_DATABASE: 'devdb',
+    },
+  };
+};
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    domains: ['img.sndimg.com', 'images.pexels.com'], // Updated from remotePatterns to domains
   },
-  }
-}
+  eslint: {
+    dirs: ['pages/tags'],
+  },
+
+};
---
+++ pages/recipeList/filters/index.jsx
@@ -0,0 +1,58 @@
+import { getByAggregation, getCategories } from '../../../utils/mongodb-utils';
+import Overlay from '../../../components/ui-utils/overlay/overlay';
+
+export async function getServerSideProps() {
+  // This is pattern to use in the aggregation method in mongodb to fetch a list of tags in
+  const patternForTags = [
+    {
+      $project: {
+        tags: true,
+      },
+    }, {
+      $unwind: {
+        path: '$tags',
+        preserveNullAndEmptyArrays: false,
+      },
+    }, {
+      $group: {
+        _id: null,
+        uniqueTags: {
+          $addToSet: '$tags',
+        },
+      },
+    },
+  ];
+
+  try {
+    const recipeDocuments = await getCategories(
+      'categories',
+    );
+    const uniqueTags = await getByAggregation(
+      'recipes',
+      patternForTags,
+    );
+    const arrayOfUnigueTags = uniqueTags[0].uniqueTags;
+
+    const categoriesArr = recipeDocuments[0].categories;
+
+    return {
+      props: {
+        categoriesArr,
+        arrayOfUnigueTags,
+      },
+    };
+  } catch (error) {
+    console.error('Getting recipes failed');
+    return {
+      notFound: true,
+    };
+  }
+}
+
+export default function Filter({ categoriesArr, arrayOfUnigueTags }) {
+  return (
+    <Overlay
+      categoriesArr={categoriesArr}
+    />
+  );
+}
