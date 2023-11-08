aafee78a8cee4c8d3acb01e92460027fe879de13
lesegomatshego <125870677+lesegomatshego@users.noreply.github.com>
2023-11-08 11:54:45 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,26 +1,26 @@
-require('dotenv').config();
-const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
-
-
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
+ 
 module.exports = (phase, { defaultConfig }) => {
   if (phase === PHASE_DEVELOPMENT_SERVER) {
     return {
       reactStrictMode: true,
       env: {
-        MONGODB_USERNAME: process.env.MONGODB_USERNAME,
-        MONGODB_PASSWORD: process.env.MONGODB_PASSWORD,
-        MONGODB_CLUSTERNAME: process.env.MONGODB_CLUSTERNAME,
-        MONGODB_DATABASE: process.env.MONGODB_DATABASE_DEV,
+        MONGODB_USERNAME: 'groupc',
+        MONGODB_PASSWORD: 'UFX6A2FjZLMhDuME',
+        MONGODB_CLUSTERNAME: 'groupc',
+        MONGODB_DATABASE: 'dev-dummy',
       },
-    };
+    }
   }
+
+
   return {
-    reactStrictMode: true,
-    env: {
-      MONGODB_USERNAME: process.env.MONGODB_USERNAME,
-      MONGODB_PASSWORD: process.env.MONGODB_PASSWORD,
-      MONGODB_CLUSTERNAME: process.env.MONGODB_CLUSTERNAME,
-      MONGODB_DATABASE: process.env.MONGODB_DATABASE,
-    },
-  };
-};
+      reactStrictMode: true,
+  env: {
+    MONGODB_USERNAME: 'groupc',
+    MONGODB_PASSWORD: 'UFX6A2FjZLMhDuME',
+    MONGODB_CLUSTERNAME: 'groupc',
+    MONGODB_DATABASE: 'devdb',
+  },
+  }
+}
