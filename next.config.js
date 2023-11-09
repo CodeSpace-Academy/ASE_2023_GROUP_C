aa98e51ebb7277e21d41bc56c25956d61ebd0ec3
LentsweLetlotloMokotedi <lentsweletlotlo0@gmail.com>
2023-11-09 14:07:55 +0200

    pulle origin

--- next.config.js
+++ next.config.js
@@ -1,49 +1,26 @@
-const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
-
-module.exports = (phase) => {
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
+ 
+module.exports = (phase, { defaultConfig }) => {
   if (phase === PHASE_DEVELOPMENT_SERVER) {
     return {
       reactStrictMode: true,
-<<<<<<< HEAD
-  env: {
-    MONGODB_USERNAME: 'groupc',
-    MONGODB_PASSWORD: 'UFX6A2FjZLMhDuME',
-    MONGODB_CLUSTERNAME: 'groupc',
-    MONGODB_DATABASE: 'devdb',
-  },
-  }
-}
-
-
-return {
-  reactStrictMode: true,
-  env: {
-    MONGODB_USERNAME: 'groupc',
-    MONGODB_PASSWORD: 'UFX6A2FjZLMhDuME',
-    MONGODB_CLUSTERNAME: 'groupc',
-    MONGODB_DATABASE: 'dev-dummy',
-  },
-}
-}
-
-=======
       env: {
-        MONGODB_USERNAME: "groupc",
-        MONGODB_PASSWORD: "UFX6A2FjZLMhDuME",
-        MONGODB_CLUSTERNAME: "groupc",
-        MONGODB_DATABASE: "dev-dummy",
+        MONGODB_USERNAME: 'groupc',
+        MONGODB_PASSWORD: 'UFX6A2FjZLMhDuME',
+        MONGODB_CLUSTERNAME: 'groupc',
+        MONGODB_DATABASE: 'dev-dummy',
       },
-    };
+    }
   }
+
 
   return {
     reactStrictMode: true,
     env: {
-      MONGODB_USERNAME: "groupc",
-      MONGODB_PASSWORD: "UFX6A2FjZLMhDuME",
-      MONGODB_CLUSTERNAME: "groupc",
-      MONGODB_DATABASE: "devdb",
-    },
-  };
-};
->>>>>>> bd7df3d7dec03e2853cb51a549e0ffc67c648021
+      MONGODB_USERNAME: 'groupc',
+      MONGODB_PASSWORD: 'UFX6A2FjZLMhDuME',
+      MONGODB_CLUSTERNAME: 'groupc',
+      MONGODB_DATABASE: 'devdb',
+    }
+  }
+}
