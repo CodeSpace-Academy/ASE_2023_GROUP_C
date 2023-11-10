fabe9f140cf66b14eb917e29716008bdb6f31d75
koketsobogopane <koketsobogopanenov24@gmail.com>
2023-11-10 09:20:05 +0200

    format indentation

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
-const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
 
 module.exports = (phase) => {
   if (phase === PHASE_DEVELOPMENT_SERVER) {
     return {
       reactStrictMode: true,
       env: {
-        MONGODB_USERNAME: "groupc",
-        MONGODB_PASSWORD: "UFX6A2FjZLMhDuME",
-        MONGODB_CLUSTERNAME: "groupc",
-        MONGODB_DATABASE: "dev-dummy",
+        MONGODB_USERNAME: 'groupc',
+        MONGODB_PASSWORD: 'UFX6A2FjZLMhDuME',
+        MONGODB_CLUSTERNAME: 'groupc',
+        MONGODB_DATABASE: 'dev-dummy',
       },
     };
   }
@@ -16,10 +16,10 @@ module.exports = (phase) => {
   return {
     reactStrictMode: true,
     env: {
-      MONGODB_USERNAME: "groupc",
-      MONGODB_PASSWORD: "UFX6A2FjZLMhDuME",
-      MONGODB_CLUSTERNAME: "groupc",
-      MONGODB_DATABASE: "devdb",
+      MONGODB_USERNAME: 'groupc',
+      MONGODB_PASSWORD: 'UFX6A2FjZLMhDuME',
+      MONGODB_CLUSTERNAME: 'groupc',
+      MONGODB_DATABASE: 'devdb',
     },
   };
 };
