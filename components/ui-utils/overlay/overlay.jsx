75fcd91efbdfe5133b90a6918ca8e26cfa9f8815
kwetso12 <kgaladikwetso@gmail.com>
2023-11-14 09:41:19 +0200

    es-lint
    applied linting

--- components/ui-utils/overlay/overlay.jsx
+++ components/ui-utils/overlay/overlay.jsx
@@ -1,8 +1,7 @@
-import { useEffect, useRef, useState } from 'react';
-import Filtering from '../../filtering/allFilter';
-import styles from './overlay.module.css'
+import { useState } from 'react';
 import { useRouter } from 'next/router';
-
+import Filtering from '../../filtering/allFilter';
+import styles from './overlay.module.css';
 
 /**
  * Overlay component for filtering recipes.
@@ -11,24 +10,21 @@ import { useRouter } from 'next/router';
  * @returns {JSX.Element} React component.
  */
 
-export default function Overlay({categoriesArr}) {
+export default function Overlay({ categoriesArr }) {
   const [data, setData] = useState({
-    categories: '', 
-    numberOfSteps: '', 
-    filterByIngredients: ''
+    categories: '',
+    numberOfSteps: '',
+    filterByIngredients: '',
   });
   // const ing/redientsInputRef = useRef();
 
-  const router = useRouter()
+  const router = useRouter();
 
-  console.log(data.filterByIngredients)
-
-  let arrayOfIngrerdients
-  function handleIngredientsChange(){
-    const ingredientsValue = data.filterByIngredients
+  let arrayOfIngrerdients;
+  function handleIngredientsChange() {
+    const ingredientsValue = data.filterByIngredients;
 
     arrayOfIngrerdients = ingredientsValue.split(' ');
-    console.log(arrayOfIngrerdients);
   }
 
   /**
@@ -49,13 +45,13 @@ export default function Overlay({categoriesArr}) {
 
   const handleOkButtonClick = () => {
     let url; // Set url of the filtered options
-    
-    if (data.numberOfSteps === ''){
-      url = `/recipeList/filters/${data.categories}`
-    } else if (data.categories === ''){
-      url = `/recipeList/filters/steps/${data.numberOfSteps}`
+
+    if (data.numberOfSteps === '') {
+      url = `/recipeList/filters/${data.categories}`;
+    } else if (data.categories === '') {
+      url = `/recipeList/filters/steps/${data.numberOfSteps}`;
     } else {
-    url = `/recipeList/filters/steps/${data.numberOfSteps}/${data.categories}`;
+      url = `/recipeList/filters/steps/${data.numberOfSteps}/${data.categories}`;
     }
 
     if (arrayOfIngrerdients && arrayOfIngrerdients.length !== 0) {
@@ -64,24 +60,32 @@ export default function Overlay({categoriesArr}) {
 
     // Use router.push to navigate to the dynamic URL
     router.push(url);
-
   };
 
-  return(
+  return (
     <div className={styles.overlay}>
       <div className={styles.dialogBox}>
         <Filtering
           categoriesArr={categoriesArr}
           data={data}
           onChange={handleInputChange}
+          // eslint-disable-next-line react/jsx-no-bind
           handleIngredientsChange={handleIngredientsChange}
         />
         <div className={styles.buttonContainer}>
-          <button className={`${styles.button} ${styles.cancelButton}`} >Cancel</button>
-          <button 
+          <button
+            type="button"
+            className={`${styles.button} ${styles.cancelButton}`}
+          >
+            Cancel
+          </button>
+          <button
+            type="button"
             className={`${styles.button} ${styles.okButton}`}
             onClick={handleOkButtonClick}
-          >FIlter</button>
+          >
+            FIlter
+          </button>
         </div>
       </div>
     </div>
