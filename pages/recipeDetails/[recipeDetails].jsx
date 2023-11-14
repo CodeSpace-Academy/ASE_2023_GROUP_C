54ea7951b4e568b4334a6bf470462f64aea2e3cf
kwetso12 <kgaladikwetso@gmail.com>
2023-11-14 15:26:19 +0200

    added es-lint
    applied es-linting

--- pages/recipeDetails/[recipeDetails].jsx
+++ pages/recipeDetails/[recipeDetails].jsx
@@ -1,10 +1,15 @@
 /* eslint-disable no-console */
+/* eslint-disable jsx-a11y/control-has-associated-label */
 /* eslint-disable no-underscore-dangle */
 import React from 'react';
 import { useRouter } from 'next/router';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
-import { getRecipeDetails, getAllergens, getFavouriteRecipes } from '../../utils/mongodb-utils';
+import {
+  getRecipeDetails,
+  getAllergens,
+  getFavouriteRecipes,
+} from '../../utils/mongodb-utils';
 import RecipeCard from '../../components/recipeCard/recipeCard';
 
 export async function getServerSideProps(context) {
@@ -13,20 +18,23 @@ export async function getServerSideProps(context) {
   let recipeDocuments;
   let allergens;
 
-  const favouriteRecipes = await getFavouriteRecipes(
-    'users-list',
-    { userName: 'The User 1' },
-  );
+  const favouriteRecipes = await getFavouriteRecipes('users-list', {
+    userName: 'The User 1',
+  });
   // An array of a favourite recipes
   const usersFavouriteLists = favouriteRecipes.userList;
   // Creating a set of recipe Id.
   // eslint-disable-next-line no-underscore-dangle
-  const favouriteRecipeIds = new Set(usersFavouriteLists.map((recipe) => recipe._id));
+  const favouriteRecipeIds = new Set(
+    usersFavouriteLists.map((recipe) => recipe._id),
+  );
 
   // If the recipe ID in context is avaible in the favourite then that
   // recipe should be returned.
   if (favouriteRecipeIds.has(recipeId)) {
-    recipeDocuments = usersFavouriteLists.find((favRecipe) => favRecipe._id === recipeId);
+    recipeDocuments = usersFavouriteLists.find(
+      (favRecipe) => favRecipe._id === recipeId,
+    );
 
     allergens = await getAllergens('allergens');
 
@@ -60,7 +68,9 @@ export default function RecipeDetails({ recipeDocuments, allergensList }) {
         <button type="button" onClick={() => router.back()}>
           <FontAwesomeIcon icon={faChevronLeft} size="xl" />
         </button>
-        <h1 className=" text-center font-bold p-5 text-xl bg-slate-900 text-white">RecipeDetails</h1>
+        <h1 className=" text-center font-bold p-5 text-xl bg-slate-900 text-white">
+          RecipeDetails
+        </h1>
       </div>
       <RecipeCard recipe={recipeDocuments} allergensList={allergensList} />
     </div>
