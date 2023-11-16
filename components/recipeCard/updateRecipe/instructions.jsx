ca806f5494ade058edb5d034820719f343e77fb8
koketsobogopane <koketsobogopanenov24@gmail.com>
2023-11-16 11:31:59 +0200

    Removed unused vars

--- components/recipeCard/updateRecipe/instructions.jsx
+++ components/recipeCard/updateRecipe/instructions.jsx
@@ -1,8 +1,5 @@
-/* eslint-disable import/no-unresolved */
 import React, { useState } from 'react';
 import { uuid } from 'uuidv4';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTriangleExclamation } from '@fortawesome/free-solid-svg-icons';
 import EditRecipeContent from './editableText';
 
 function RecipeInstruction(props) {
