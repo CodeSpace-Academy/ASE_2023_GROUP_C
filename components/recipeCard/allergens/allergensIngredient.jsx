d9fca24dabf7897d57d17876d5e9c9aad8712fba
Kopo Ntheledi <kopontheledi@gmail.com>
2023-11-21 11:50:41 +0200

    removed the uuidv4, fixed the allergens and also replaced the uuidv4 with v4

--- components/recipeCard/allergens/allergensIngredient.jsx
+++ components/recipeCard/allergens/allergensIngredient.jsx
@@ -1,12 +1,14 @@
 export default function Allergens({ recipe, allergensList }) {
   const { ingredients } = recipe;
 
-  if (Array.isArray(allergensList)) {
-    const ingredientsWithAllergensInRecipe = Object.keys(ingredients)
-      .filter((key) => allergensList.some((allergen) => key.toLowerCase().includes(allergen)));
+  const ingredientsWithAllergensInRecipe = Object.keys(ingredients)
+    .filter((key) => allergensList.some((allergen) => key.toLowerCase().includes(allergen)));
 
-    return <div>{ingredientsWithAllergensInRecipe.join(', ')}</div>;
-  }
-
-  return null; // Return null or an appropriate value when allergensList is not an array.
+  return (
+    <div>
+      {ingredientsWithAllergensInRecipe.length > 0
+        ? ingredientsWithAllergensInRecipe.join(', ')
+        : 'No allergens'}
+    </div>
+  );
 }
