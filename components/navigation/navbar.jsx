3d81f94025b8cd092d320ed6043bfd1494968251
Kealeboga Letlhake <letlhake.kv@gmail.com>
2023-11-27 09:13:53 +0200

    Update Navbar

--- components/navigation/navbar.jsx
+++ components/navigation/navbar.jsx
@@ -5,15 +5,31 @@ import NavLinks from './navlinks';
 import MobileMenuButton from './mobileMenuButton';
 import HomeButton from './homeButton';
 
+/**
+ * Responsive navigation bar component.
+ * The NavBar component includes a home button, search functionality with a search bar,
+ * navigation links, and a mobile menu for smaller screens.
+ *
+ */
+
 function NavBar() {
+  // State hooks for managing component state
   const [query, setQuery] = useState('');
   const [isOpen, setIsOpen] = useState(false);
   const [isSearchBarVisible, setIsSearchBarVisible] = useState(false);
 
+  /**
+   * Toggles the mobile navigation menu.
+   * @function
+   */
   const toggleNav = () => {
     setIsOpen(!isOpen);
   };
 
+  /**
+   * Toggles the visibility of the search bar.
+   * @function
+   */
   const toggleSearchBar = () => {
     setIsSearchBarVisible(!isSearchBarVisible);
   };
@@ -22,16 +38,21 @@ function NavBar() {
     <div className="relative z-50">
       <div className="bg-gray-800 p-4 fixed w-full top-0">
         <div className="flex items-center justify-between">
+          {/* Home button */}
           <HomeButton />
+          {/* Search icon and search bar */}
           <div className="flex items-center space-x-2 text-white p-2 flex flex-wrap justify-center gap-1 border-slate-500 border rounded-lg items-center md:justify-between">
             <SearchIcon onClick={toggleSearchBar} />
             {isSearchBarVisible && <SearchBar setQuery={setQuery} />}
           </div>
+          {/* Navigation links for larger screens */}
           <div className="hidden md:flex items-center space-x-2 text-white">
             <NavLinks />
           </div>
+          {/* Mobile menu button */}
           <MobileMenuButton isOpen={isOpen} onClick={toggleNav} />
         </div>
+        {/* Mobile menu */}
         {isOpen && (
           <div className="bg-gray-900 text-white p-2 mt-2 md:hidden">
             <NavLinks query={query} />
