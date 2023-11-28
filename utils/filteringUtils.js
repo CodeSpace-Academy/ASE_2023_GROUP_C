a83b12b3b8ce6677b5a12f6c171f418266fac444
keotshepileMaje <keotshepilemaje@gmail.com>
2023-11-28 15:17:02 +0200

    Rremove code

--- utils/filteringUtils.js
+++ utils/filteringUtils.js
@@ -35,37 +35,3 @@ export function sortingByFunction(sortingBy) {
   // Use the sortingBy value to get the corresponding sorting object
   return sortingOptions[sortingBy];
 }
-
-export function filteringObjectFunction(filter, search) {
-  const mongoFilterObject = {};
-
-  if (search){
-    mongoFilterObject.title = { $regex: search, $options: 'i' }
-  } else {
-    if (filter.categories) {
-      mongoFilterObject.category = { $in: [filter.categories] };
-    }
-    if (filter.tags) {
-      mongoFilterObject.tags = { $in: [filter.tags] };
-    }
-    if (filter.numberOfSteps) {
-      mongoFilterObject.instructions = {
-        $size: parseInt(filter.numberOfSteps, 10),
-      };
-    }
-    if (filter.filterByIngredients) {
-      // The filterArray generate a list of object that searches in mongodb.
-      const filterArray = filter.filterByIngredients
-        .slice(1)
-        .map((ingredient) => {
-          const key = `ingredients.${ingredient}`;
-          return { [key]: { $exists: true } };
-        });
-
-      if (filterArray.length > 0) {
-        mongoFilterObject.$and = filterArray;
-      }
-    }
-  }
-  return mongoFilterObject
-}
