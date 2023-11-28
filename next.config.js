71715ca33680742299c7e7b6d26fc8dbe3502890
LentsweLetlotloMokotedi <lentsweletlotlo0@gmail.com>
2023-11-28 09:14:01 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,32 +1,21 @@
+// next.config.js
 const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
-
-module.exports = {
-  eslint: {
-    dirs: ['pages/index.jsx'],
-  },
-};
-
+require('dotenv').config(); // Load environment variables from .env.local
 
 module.exports = (phase) => {
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    return {
-      reactStrictMode: true,
-      env: {
-        MONGODB_USERNAME: "groupc",
-        MONGODB_PASSWORD: "UFX6A2FjZLMhDuME",
-        MONGODB_CLUSTERNAME: "groupc",
-        MONGODB_DATABASE: "devdb",
-      },
-    };
-  }
+  const isDevelopment = process.env.PHASE_DEVELOPMENT_SERVER === 'true';
+
+  const mongodbDatabase = isDevelopment
+    ? process.env.MONGODB_DATABASE_DEV
+    : process.env.MONGODB_DATABASE_PROD;
 
   return {
     reactStrictMode: true,
     env: {
-      MONGODB_USERNAME: 'groupc',
-      MONGODB_PASSWORD: 'UFX6A2FjZLMhDuME',
-      MONGODB_CLUSTERNAME: 'groupc',
-      MONGODB_DATABASE: 'devdb',
+      MONGODB_USERNAME: process.env.MONGODB_USERNAME,
+      MONGODB_PASSWORD: process.env.MONGODB_PASSWORD,
+      MONGODB_CLUSTERNAME: process.env.MONGODB_CLUSTERNAME,
+      MONGODB_DATABASE: mongodbDatabase,
     },
   };
 };
