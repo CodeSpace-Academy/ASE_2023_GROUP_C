9f684060f665ba914383422a18e9008723cfecc9
koketsobogopane <koketsobogopanenov24@gmail.com>
2023-11-30 09:02:40 +0200

    Update message

--- components/layout/noResultsMessage.jsx
+++ components/layout/noResultsMessage.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function NoResultsMessage() {
   return (
     <p className="bg-gray-800 text-white p-4">
-      No results found.Please try a different query.
+      Oops!No results are matching your query.
     </p>
   );
 }
